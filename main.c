3c4e88a793d41e97edb5390ecb289deca8d07613
codeplusmath <rajkumarsawant001@gmail.com>
2022-11-10 21:59:28 +0530

    Change in main.c for Jenkins

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 int main(int argc, char * argv[]){
 	int a=atoi(argv[1]), b=atoi(argv[2]);
+    printf("This is change in main.c for jenkins auto build pipeline\n");
 	printf("Addition:\n\t%d + %d = %d\n", a, b, a+b);
 	return 0;
 }
